bd8583c6ce0bb98a22a922bf20741a4e07fe8f77
mini <jeffrey.sprenger@gmail.com>
2025-01-31 19:53:03 +0100

    Add NEON

--- src/drizzle/index.ts
+++ src/drizzle/index.ts
@@ -1,12 +1,19 @@
-import { drizzle } from 'drizzle-orm/better-sqlite3';
-import Database from 'better-sqlite3';
-import * as schema from './schema';
+// import { drizzle } from 'drizzle-orm/better-sqlite3';
+// import Database from 'better-sqlite3';
+// import * as schema from './schema';
 
-// const client = createClient({
-//   url: process.env.TURSO_CONNECTION_URL!,
-//   authToken: process.env.TURSO_AUTH_TOKEN!,
-// });
+// // const client = createClient({
+// //   url: process.env.TURSO_CONNECTION_URL!,
+// //   authToken: process.env.TURSO_AUTH_TOKEN!,
+// // });
 
-const sqlite = new Database('./test.db');
+// const sqlite = new Database('./test.db');
 
-export const db = drizzle(sqlite, { schema: { ...schema } });
+// export const db = drizzle(sqlite, { schema: { ...schema } });
+
+// src/db.ts
+import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
+
+const sql = neon(process.env.DATABASE_URL!);
+export const db = drizzle({ client: sql });
