e67de3ee11cb604fa659b0934e563368d25bb90b
dovonun <31408384+Dovonun@users.noreply.github.com>
2025-01-31 19:22:17 +0100

    add new pg schema in ts

--- drizzle.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'drizzle-kit'
-
-export default defineConfig({
-    schema: "./src/drizzle/schema.js",
-    dialect: 'sqlite',
-    dbCredentials: {
-        url: './test.db',
-    }
-});
