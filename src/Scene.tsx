5d78b4ff978f8ed6fb179bae2c07466f0509929f
mini <jeffrey.sprenger@gmail.com>
2025-06-27 13:13:34 +0200

    Normal test

--- src/Scene.tsx
+++ src/Scene.tsx
@@ -2940,11 +2940,11 @@ const TextBox = ({ name, text, speeed, show, tb}) => {
               
               //validateToken();
               
-              if (/Mobi|Android/i.test(navigator.userAgent)){
-                  //console.log('Moblie user');
-                  setContent(await load("Extra", 'mobile'));
-                  setFrame(0);
-              } else {
+              // if (/Mobi|Android/i.test(navigator.userAgent)){
+              //     //console.log('Moblie user');
+              //     setContent(await load("Extra", 'mobile'));
+              //     setFrame(0);
+              // } else {
 
                 
               //const trgt = localStorage.getItem('CurrentChapter');
@@ -2979,7 +2979,6 @@ const TextBox = ({ name, text, speeed, show, tb}) => {
             //frame.items.item
             
             reset();
-          }
           } else if (scene.next.type === "startup"){
             if (EventFV !== 'NS'){
               //Preload not needed ig
